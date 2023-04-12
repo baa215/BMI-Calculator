1f83e7b6f53dc57e01b32d03d853897ccc745ea6
baa215 <77748233+baa215@users.noreply.github.com>
2023-04-11 21:35:59 -0500

    Update BMIcalculator.py

--- BMIcalculator.py
+++ BMIcalculator.py
@@ -1,33 +1,32 @@
-import sys
 import math
+import sys
 
 def heightcal(ft, inches): # calculates the height into inches 
     heighttotal = (ft * 12) + inches
     return heighttotal
 
 def bmical(weight, height): # gets the bmi 
-
     bmi = weight / (height)
     return bmi
 
-height_ft = float(sys.argv[1])
-height_in = float(sys.argv[2])
-weight = float(sys.argv[3])
+if __name__ == "__main__":
+    height_ft = float(sys.argv[1])
+    height_in = float(sys.argv[2])
+    weight_pounds = float(sys.argv[3])
+    
+    height = (heightcal(height_ft, height_in) * 0.025)**2 # does the conversion to meters 
+    weight = weight_pounds * 0.45 # does the coversion to metric system 
+    BMI = bmical(weight, height) # calls the bmi cal
 
-height = ((heightcal(height_ft, height_in) * 0.025)**2) # does the conversion to meters 
-weight = weight * 0.45 # does the coversion to metric system 
+    print("Your BMI is: ")
+    print(round(BMI)) # makes bmi a whole number 
 
-BMI = bmical(weight, height) # calls the bmi cal
-
-print("Your BMI is: ")
-print(round(BMI)) # makes bmi a whole number 
-
-if(BMI>0): # compares bmi to all of these to find the correct one
-    if(BMI<=18.5):
-        print("You are underweight.")
-    elif(BMI<=25):
-        print("You are the normal weight.")
-    elif(BMI<=30):
-        print("You are overweight.")
-    else: 
-        print("You are obese.")
+    if(BMI>0): # compares bmi to all of these to find the correct one
+        if(BMI<=18.5):
+            print("You are underweight.")
+        elif(BMI<=25):
+            print("You are the normal weight.")
+        elif(BMI<=30):
+            print("You are overweight.")
+        else: 
+            print("You are obese.")
